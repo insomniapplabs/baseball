79cc8750f13b3d40eb207de24e573456cef7d71e
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-02-18 13:14:24 -0600

    add routes and endpoints

---
+++ client/app/dashboard/dashboard.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('baseballApp')
+  .controller('DashboardCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/home/home.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('baseballApp')
+  .controller('HomeCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
