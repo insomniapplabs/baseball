8ecd6ca7a1cce1c1a66c5fea29551e1d8e29cca4
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-02-21 11:31:33 -0600

    add calendar

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('baseballApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ui.calendar'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
--- client/app/events/events.controller.js
+++ client/app/events/events.controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('baseballApp')
-  .controller('EventsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('EventsCtrl', function ($scope, $http) {
+
+    $scope.events = [];
+
+    $http.get('api/events').success(function(events) {
+      $scope.events = events;
+    });
+
+    $scope.eventSource = {url: '/api/events'};
+
+    $scope.eventSources = [$scope.events, $scope.eventSource];
+
+
+
   });
