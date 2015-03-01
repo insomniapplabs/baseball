2a9422da0b9dcbe0963b1c11483a5f35878d1d01
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-02-28 18:03:32 -0600

    new main page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,37 @@
 
 angular.module('baseballApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    
+    $scope.events = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    $http.get('/api/events').success(function(events) {
+      $scope.events = events;
+      socket.syncUpdates('event', $scope.events);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
 
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
+
+    //$scope.awesomeThings = [];
+//
+    //$http.get('/api/things').success(function(awesomeThings) {
+    //  $scope.awesomeThings = awesomeThings;
+    //  socket.syncUpdates('thing', $scope.awesomeThings);
+    //});
+//
+    //$scope.addThing = function() {
+    //  if($scope.newThing === '') {
+    //    return;
+    //  }
+    //  $http.post('/api/things', { name: $scope.newThing });
+    //  $scope.newThing = '';
+    //};
+//
+    //$scope.deleteThing = function(thing) {
+    //  $http.delete('/api/things/' + thing._id);
+    //};
+//
+    //$scope.$on('$destroy', function () {
+    //  socket.unsyncUpdates('thing');
+    //});
   });
