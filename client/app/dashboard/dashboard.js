5428a9f6e302d5c72a75190815720129da632a79
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-03-05 20:10:11 -0600

    add authentication

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -6,36 +6,43 @@ angular.module('baseballApp')
       .state('dashboard', {
         url: '/dashboard',
         templateUrl: 'app/dashboard/dashboard.html',
-        controller: 'DashboardCtrl'
+        controller: 'DashboardCtrl',
+        authenticate: true
       })
       .state('dashboard.home', {
         url: '/home',
         templateUrl: 'app/dashboard/home.html',
-        controller: 'DashboardCtrl'
+        controller: 'DashboardCtrl',
+        authenticate: true
       })
       .state('dashboard.rosters', {
         url: '/rosters',
         templateUrl: 'app/dashboard/rosters.html',
-        controller: 'RostersCtrl'
+        controller: 'RostersCtrl',
+        authenticate: true
       })
       .state('dashboard.updates', {
         url: '/updates',
         templateUrl: 'app/dashboard/updates.html',
-        controller: 'UpdatesController'
+        controller: 'UpdatesController',
+        authenticate: true
       })
       .state('dashboard.newUpdate', {
         url: '/updates/new',
         templateUrl: 'app/dashboard/new-update.html',
-        controller: 'UpdatesController'
+        controller: 'UpdatesController',
+        authenticate: true
       })
       .state('dashboard.coaches', {
         url: '/coaches',
         templateUrl: 'app/dashboard/coaches.html',
-        controller: 'CoachesCtrl'
+        controller: 'CoachesCtrl',
+        authenticate: true
       })
       .state('dashboard.events', {
         url: '/events',
         templateUrl: 'app/dashboard/events.html',
-        controller: 'EventsController'
+        controller: 'EventsController',
+        authenticate: true
       });
   });
