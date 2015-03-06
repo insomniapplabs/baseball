dbc3045f6b7f8a582811cf2d1a041e3dcecc8fb9
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-03-05 20:42:34 -0600

    add login

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -15,7 +15,7 @@ angular.module('baseballApp')
         })
         .then( function() {
           // Logged in, redirect to home
-          $location.path('/');
+          $location.path('/dashboard/home');
         })
         .catch( function(err) {
           $scope.errors.other = err.message;
